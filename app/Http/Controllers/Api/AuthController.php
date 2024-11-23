dbc665a73547fece1ebf7bac49ee920cbb69171e
Janet Awino <janetkeyo@gmail.com>
2024-11-23 16:27:43 +0300

    creating RESTful Endpoints

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+
+    /**
+     * Register new user
+     * @param
+     */
+    public function register(RegisterRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(RegisterRequest $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
